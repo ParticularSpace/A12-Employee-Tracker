ed5b4d21957f4eee12574d0a8dc9419dfe7b177e
Sam Jones <samejones2018@gmail.com>
2023-04-25 17:35:19 -0700

    created my schema for department, role, and employee

--- schemas.sql
+++ schemas.sql
@@ -1,15 +1,27 @@
 DROP DATABASE IF EXISTS `employees`;
 CREATE DATABASE IF NOT EXISTS `employees`;
 
+USE `employees`;
+
 CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE role (
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
